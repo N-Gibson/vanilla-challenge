09f5d05360a8ada32ec5ed6a9f1b9e5e335a78dd
Noah Gibson <noah.gibson99@gmail.com>
2019-10-17 21:38:43 -0600

    Add styles and some logic to get color in center button

--- main.js
+++ main.js
@@ -21,19 +21,29 @@ exerciseButton.addEventListener('click', () => {
   toggleButtonStates(exerciseButton, 'exercise')
 })
 
-startButton.addEventListener('click', () => {
+startButton.addEventListener('click', (e) => {
+  let color;
+
   if(goal.value === '' || minutes.value === '' || seconds.value === '') {
     timerDiv.insertAdjacentHTML('afterbegin', '<p class="error">Please fill out all the inputs</p>')
   } else {
     // document.querySelector('.error').remove();
     form.style.display = 'none';
-    timerDiv.insertAdjacentHTML('afterbegin', `<h2>${goal.value}</h2><h3>${minutes.value}:${seconds.value}</h3><button type="button" id="start-timer-button">START</button>`)
+    timerDiv.insertAdjacentHTML('afterbegin', `<h2 class='timer-h2'>${goal.value}</h2><h3 class='timer-h3'>${minutes.value}:${seconds.value}</h3><button type="button" id="start-timer-button">START</button>`)
+    let buttons = document.querySelectorAll('button');
+    let colorPicker = Array.from(buttons).filter(button => button.classList[0] === 'category-buttons').map(button => button.dataset).forEach(button => {
+      if(button.active === 'true') {
+        color = button.color
+      }
+    })
   }
 })
 
 main.addEventListener('click', (e) => {
+  let validMins = (Number(minutes.value)*60);
+  let validSecs = Number(seconds.value);
   if(e.target.id === 'start-timer-button') {
-    startTimer(minutes.value, seconds.value)
+    startTimer(validMins, validSecs, e)
   }
 })
 
@@ -47,19 +57,18 @@ function toggleButtonStates(button, buttonName) {
   button.dataset.active === 'true' ? button.classList.add(`${buttonName}-button-active`) : button.classList.remove(`${buttonName}-button-active`)
 }
 
-function startTimer(minutes, seconds) {
-  let validMins = (Number(minutes)*60);
-  let validSecs = Number(seconds);
-  timer = document.getElementById('timer');
-  timeLeft = validMins + validSecs;
+function startTimer(minutes, seconds, e) {
+  timeRemaining = e.target.parentElement.children[1]
+  timeLeft = minutes + seconds;
   timerId = setInterval(countdown, 1000);
 }
 
 function countdown() {
   if (timeLeft === -1) {
     clearTimeout(timerId);
+    alert('Times Up!');
   } else {
-    timerDiv.innerHTML = `${timeLeft}`;
+    timeRemaining.innerHTML = `${timeLeft}`;
     timeLeft--;
   }
 }
